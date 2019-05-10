fcbc9962b047714996a0338b167dec14b36e3ebf
ezgi balıkcı <ezgibalikci7@gmail.com>
2019-05-10 16:18:11 +0300

    Faktoriyelin iki farklı şekilde çözümü yapıldı. Özyinelemeli şekilde yapıldı.

---
+++ ConsoleApp1/Maths.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Maths
+    {
+       public static int Factorial(int sonuc, int sayi)
+        {
+            for (int i = 1; i <= sayi; i++)
+            {
+                sonuc = sonuc * i;
+            }
+            return sonuc;
+
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        public static int Fibonacci(int n)
+        {
+            int fi1 = 1, fi2 = 2;
+            if (n == 1)
+            {
+                return fi1;
+            }
+            else if (n == 2)
+            {
+
+                return fi2;
+
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sonuc=1;
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.Write(Maths.Fibonacci(sayi));
+            Console.Write(Maths.Factorial(sonuc, sayi));
+            Console.Write(Maths.Factorial(sayi));
+            Console.ReadKey();
+           
+        }
+       
+
+
+
+    }
+}
